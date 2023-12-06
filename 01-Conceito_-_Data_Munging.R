9eba5fe2c8ad1536398a3707f84e5534456bd081
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-12-06 16:56:25 -0300

    conceito sobre o que e Data Munging

---
+++ 01-Conceito_-_Data_Munging.R
@@ -0,0 +1,33 @@
+# Conceito - Data Munging
+
+# Configurando o diretório de trabalho
+setwd("C:/Users/Julia/Desktop/CienciaDeDados/1.Big-Data-Analytics-com-R-e-Microsoft-Azure-Machine-Learning/13.Data-Munging-no-Azure-Machine-Learning")
+getwd()
+
+
+
+## O que é Data Munging ?
+
+# - Data Munging é o processo de manipulação dos dados antes de treinar um Modelo de Machine Learning
+
+
+
+## Quais tipos de Data Munging aplicaremos no Azure ML
+
+# - Execução de Scripts R no Azure ML
+# - Módulos de Manipulação
+# - Tratamento de Metadados
+# - Tratamento de Dados Duplicados e Missing
+# - Transformação e Padronização
+# - Erros e Outliers
+# - SQL no Azure ML
+# - Utilização de Joins para Combinação de Datasets
+# - Pacotes dplyr e tidyr
+
+# E também...
+
+# - Criação de Gráficos com o pacote ggplot2 no Azure ML
+
+
+
+
